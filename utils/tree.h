b008293993dbe888f52bb88d90ed2f8c895472b8
Qin Fandong <shell_way@foxmail.com>
2021-12-09 22:40:28 +0800

    Rewrite arrayFromTree with recursion

--- utils/tree.h
+++ utils/tree.h
@@ -3,8 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "utstack.h"
-#include "linked_list.h"
 
 typedef struct TreeNode {
   int val;
@@ -12,28 +10,18 @@ typedef struct TreeNode {
   struct TreeNode * right;
 } TreeNode_t;
 
-typedef struct element {
-  TreeNode_t * node;
-  struct element * next;
-} element_t;
-
-inline static void treeFromArray(TreeNode_t ** root, const int * const pArray, const size_t index,
-                                 const size_t length)
+inline static int deepOfTree(const TreeNode_t * const root)
 {
-  if (index > length - 1) {
-    return;
+  int left = 0, right = 0;
+
+  if (!root) {
+    return 0;
   }
 
-  if (0 != pArray[index]) {
-    *root = (TreeNode_t *)malloc(sizeof(TreeNode_t));
+  left = deepOfTree(root->left);
+  right = deepOfTree(root->right);
 
-    (*root)->val = pArray[index];
-    (*root)->left = NULL;
-    (*root)->right = NULL;
-  }
-
-  treeFromArray(&(*root)->left, pArray, index * 2 + 1, length);
-  treeFromArray(&(*root)->right, pArray, index * 2 + 2, length);
+  return (left > right ? left : right) + 1;
 }
 
 inline static void freeTree(TreeNode_t * root)
@@ -54,53 +42,67 @@ inline static void freeTree(TreeNode_t * root)
   root = NULL;
 }
 
-inline static void arrayFromTree(int * pArray, TreeNode_t * root, size_t index)
+inline static void treeFromArray(TreeNode_t ** const root, const int * const pArray, const size_t index,
+                                 const size_t length)
 {
-  element_t * stack = NULL, * temp = NULL, * el = NULL;
-
-  if(!root) {
+  if (index > length - 1) {
     return;
   }
 
+  if (0 != pArray[index]) {
+    *root = (TreeNode_t *)malloc(sizeof(TreeNode_t));
+
+    (*root)->val = pArray[index];
+    (*root)->left = NULL;
+    (*root)->right = NULL;
+  }
+
+  treeFromArray(&(*root)->left, pArray, index * 2 + 1, length);
+  treeFromArray(&(*root)->right, pArray, index * 2 + 2, length);
+}
+
+inline static void arrayFromTreeLevel(int * const pArray, TreeNode_t * const root, size_t * const index,
+                                      const size_t level)
+{
+  if (!root) {
+    return;
+  }
+
+  if (0 == level) {
+    if (root->left) {
+      pArray[(*index)++] = root->left->val;
+    } else {
+      if (root->right) {
+        pArray[(*index)++] = 0;
+      }
+    }
+
+    if (root->right) {
+      pArray[(*index)++] = root->right->val;
+    } else {
+      if (root->left) {
+        pArray[(*index)++] = 0;
+      }
+    }
+  }
+
+  arrayFromTreeLevel(pArray, root->left, index, level - 1);
+  arrayFromTreeLevel(pArray, root->right, index, level - 1);
+}
+
+inline static void arrayFromTree(int * const pArray, TreeNode_t * const root, size_t index)
+{
+  int deep = 0;
+
+  if (!root) {
+    return;
+  }
+
+  deep = deepOfTree(root);
   pArray[index++] = root->val;
 
-  el = (element_t *)malloc(sizeof(element_t));
-  el->node = root;
-  STACK_PUSH(stack, el);
-
-  while (!STACK_EMPTY(stack)) {
-    STACK_POP(stack, temp);
-
-    if (temp->node->left) {
-      pArray[index++] = temp->node->left->val;
-    } else {
-      if (temp->node->right) {
-        pArray[index++] = 0;
-      }
-    }
-
-    if (temp->node->right) {
-      pArray[index++] = temp->node->right->val;
-    } else {
-      if (temp->node->left) {
-        pArray[index++] = 0;
-      }
-    }
-
-    if (temp->node->right) {
-      el = (element_t *)malloc(sizeof(element_t));
-      el->node = temp->node->right;
-      STACK_PUSH(stack, el);
-    }
-
-    if (temp->node->left) {
-      el = (element_t *)malloc(sizeof(element_t));
-      el->node = temp->node->left;
-      STACK_PUSH(stack, el);
-    }
-
-    free(temp);
-    temp = NULL;
+  for (size_t i = 0; i < (size_t)deep - 1; ++i) {
+    arrayFromTreeLevel(pArray, root, &index, i);
   }
 }
 
